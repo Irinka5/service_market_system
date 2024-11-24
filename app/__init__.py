63d2bc1f93d431537af93dde645ba6838cd75b75
Odin-SG <yuga1994@mail.ru>
2024-11-24 17:59:00 +0300

    Добавлены base и index

--- app/__init__.py
+++ app/__init__.py
@@ -8,15 +8,12 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 migrate = Migrate()
 
-
 # Функция для создания экземпляра приложения
 def create_app():
     app = Flask(__name__)
 
     # Конфигурация приложения
-    app.config['SECRET_KEY'] = 'your_secret_key_here'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object('app.config.Config')
 
     # Инициализация расширений с приложением
     db.init_app(app)
@@ -27,7 +24,7 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.login_message_category = 'info'
 
-    # Регистрация Blueprints (модулей)
+    # Регистрация Blueprints
     from .auth import auth as auth_blueprint
     from .main import main as main_blueprint
     app.register_blueprint(auth_blueprint)
