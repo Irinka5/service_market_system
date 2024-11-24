7bef923d379f87cf1f78a5f45b9840c27d5feee1
Odin-SG <yuga1994@mail.ru>
2024-11-24 17:57:42 +0300

    Добавлены роуты

---
+++ app/auth/routes.py
@@ -0,0 +1,39 @@
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from ..forms import RegistrationForm, LoginForm
+from ..models import User
+from .. import db
+from . import auth
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash('Вы успешно вошли в систему.', 'success')
+            return redirect(url_for('main.index'))
+        else:
+            flash('Неверное имя пользователя или пароль.', 'danger')
+    return render_template('login.html', form=form)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='pbkdf2:sha256', salt_length=8)
+        new_user = User(username=form.username.data, password=hashed_password, role='client')
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Регистрация прошла успешно! Теперь вы можете войти.', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы успешно вышли из системы.', 'success')
+    return redirect(url_for('main.index'))
---
+++ app/main/routes.py
@@ -0,0 +1,73 @@
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_required, current_user
+from .. import db
+from ..models import Service, Application
+from ..forms import ApplicationForm
+from . import main
+
+
+@main.route('/')
+def index():
+    services = Service.query.all()
+    return render_template('index.html', services=services)
+
+@main.route('/application/new', methods=['GET', 'POST'])
+@login_required
+def new_application():
+    form = ApplicationForm()
+    services = Service.query.all()
+    form.service_id.choices = [(service.id, service.name) for service in services]
+
+    if form.validate_on_submit():
+        application = Application(user_id=current_user.id, service_id=form.service_id.data)
+        db.session.add(application)
+        db.session.commit()
+        flash('Ваша заявка успешно создана!', 'success')
+        return redirect(url_for('main.applications'))
+
+    return render_template('application_form.html', form=form)
+
+@main.route('/applications')
+@login_required
+def applications():
+    if current_user.role == 'client':
+        applications = Application.query.filter_by(user_id=current_user.id).all()
+    elif current_user.role in ['manager', 'admin']:
+        applications = Application.query.all()
+    else:
+        applications = []
+    return render_template('application_list.html', applications=applications)
+
+@main.route('/application/<int:application_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_application(application_id):
+    application = Application.query.get_or_404(application_id)
+    if current_user.role not in ['manager', 'admin']:
+        flash('У вас нет прав на редактирование этой заявки.', 'danger')
+        return redirect(url_for('main.applications'))
+
+    form = ApplicationForm()
+    services = Service.query.all()
+    form.service_id.choices = [(service.id, service.name) for service in services]
+
+    if form.validate_on_submit():
+        application.service_id = form.service_id.data
+        application.status = form.status.data
+        db.session.commit()
+        flash('Заявка успешно обновлена!', 'success')
+        return redirect(url_for('main.applications'))
+
+    return render_template('application_form.html', form=form)
+
+@main.route('/application/<int:application_id>/delete', methods=['POST'])
+@login_required
+def delete_application(application_id):
+    application = Application.query.get_or_404(application_id)
+    if current_user.role not in ['manager', 'admin']:
+        flash('У вас нет прав на удаление этой заявки.', 'danger')
+        return redirect(url_for('main.applications'))
+
+    db.session.delete(application)
+    db.session.commit()
+    flash('Заявка успешно удалена.', 'success')
+    return redirect(url_for('main.applications'))
