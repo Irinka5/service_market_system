9834bf3116e5b68a0b0d5c08e35d18266823eeb5
Odin-SG <yuga1994@mail.ru>
2024-11-24 18:16:15 +0300

    Добавлены login и register

--- app/__init__.py
+++ app/__init__.py
@@ -30,4 +30,13 @@ def create_app():
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(main_blueprint)
 
+    with app.app_context():
+        db.create_all()
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     return app
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from app import create_app
+from app import create_app, db
 
 # Создание экземпляра приложения
 app = create_app()
